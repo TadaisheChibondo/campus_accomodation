19b8350d7675b026d2ce8951e8cab519da33a822
Tadaishe Chibondo <tadaishechibondo@gmail.com>
2026-01-09 22:22:03 +0200

    Add emergency password reset

--- backend/urls.py
+++ backend/urls.py
@@ -19,6 +19,31 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+
+
+def emergency_reset(request):
+    try:
+        # checks if user exists, if not, it creates one
+        admin_user, created = User.objects.get_or_create(username='admin')
+        
+        # Force the user to be an admin with a specific password
+        admin_user.set_password('campus123')
+        admin_user.email = 'admin@campus.com'
+        admin_user.is_staff = True
+        admin_user.is_superuser = True
+        admin_user.save()
+        
+        if created:
+            return HttpResponse("Success! Created NEW superuser 'admin' with password 'campus123'")
+        else:
+            return HttpResponse("Success! Reset password for EXISTING 'admin' to 'campus123'")
+            
+    except Exception as e:
+        return HttpResponse(f"Error: {str(e)}")
+ 
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,8 +51,13 @@ urlpatterns = [
     path('api/', include('listings.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'), # LOGIN
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('emergency-reset/', emergency_reset),
 
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+# In backend/urls.py
+   
